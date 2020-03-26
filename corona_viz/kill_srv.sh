8e2b9168ab6d90d54476e63e9c16e227e7b838d9
Teo Restrepo <mateini@gmail.com>
2020-03-26 10:48:38 -0500

    minor fix

--- corona_viz/kill_srv.sh
+++ corona_viz/kill_srv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -aux | grep "gunic.*$PORT.*avm" | grep -v "grep gunic"  | awk -F ' ' '{print $2 }' | xargs -I {} kill {}
+ps -aux | grep "gunic.*corona" | grep -v "grep gunic"  | awk -F ' ' '{print $2 }' | xargs -I {} kill {}
