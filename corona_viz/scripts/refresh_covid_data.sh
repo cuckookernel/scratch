bcce03a2719c661865b223cb345c9dbf47e016fe
Teo Restrepo <mateini@gmail.com>
2020-03-26 11:13:08 -0500

    minor

--- corona_viz/scripts/refresh_covid_data.sh
+++ corona_viz/scripts/refresh_covid_data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+LOG_FILE=/home/ubuntu/scratch/corona_viz/refresh.log
+
 cd /home/ubuntu/COVID-19 || exit 1
 NOW=$(date '+%Y%m%d %H%M')
-(printf "\n\n%s" "$NOW"; git pull) >> /home/ubuntu/scratch/refresh.log 2>&1
+(printf "\n\n%s" "$NOW"; git pull) >> "$LOG_FILE" 2>&1
 
 . "/home/ubuntu/miniconda3/etc/profile.d/conda.sh"
 conda activate bokeh2
@@ -10,4 +12,4 @@ cd /home/ubuntu/scratch || exit
 export PYTHONPATH=./
 export COVID_DATA_BASE=/home/ubuntu/COVID-19
 
-python corona_viz/gen_parquet.py >> /home/ubuntu/scratch/refresh.log 2>&1
+python corona_viz/gen_parquet.py >>  "$LOG_FILE" 2>&1
