a51e5d6f3f57522768cedde0f6c6554898422240
Teo Restrepo <mateini@gmail.com>
2020-10-20 13:45:35 -0500

    publish returns

--- mp_streams/publish_btc_cop.py
+++ mp_streams/publish_btc_cop.py
@@ -2,6 +2,7 @@
 https://www.buda.com/api/v2/markets/btc-cop/ticker
 """
 
+from typing import Optional
 import os
 import sys
 import json
@@ -10,14 +11,14 @@ import logging as log
 from microprediction.polling import MicroPoll
 import requests
 
-# log.basicConfig(filename='publish_btc_cop.log', level=log.INFO)
-log.basicConfig(stream=sys.stderr, level=log.INFO)
+log.basicConfig(filename='publish_btc_cop.log', level=log.INFO)
+# log.basicConfig(stream=sys.stderr, level=log.INFO)
 log.info('Publishing starts')
 
 
 class _Config:
-    keys_file = os.getenv('HOME') + '/keys.json'
-    stream_name = 'cck-btc-cop.json'
+    keys_file = os.getenv('HOME') + '/stream_keys.json'
+    stream_name = 'cck-btc-cop-10m-ret.json'
     ticker_url = 'https://www.buda.com/api/v2/markets/btc-cop/ticker'
     interval = 10  # minutes
     key_server_url = 'https://localhost:8000/key?client_name=cck-btc-cop'
@@ -48,31 +49,43 @@ class MicroPollV2( MicroPoll ):
 def main():
     """get key, instantiate feed, run it"""
 
-    write_key = _get_key( CFG.stream_name, CFG.keys_file )
+    write_key = _get_write_key( CFG.stream_name, CFG.keys_file )
     assert write_key, f"Failed getting key for `{CFG.stream_name}` from file: {CFG.keys_file}"
+
+    price_getter = PriceGetter()
 
     feed = MicroPollV2( name=CFG.stream_name,
                         write_key=write_key,
-                        func=get_price,
+                        func=price_getter.get_return,
                         interval=CFG.interval)
 
     feed.run()
 
 
-def get_price():
-    """Query ticker url return last price"""
-    last_price = None
-    try:
-        response = requests.get(CFG.ticker_url)
-        last_price = float( response.json()['ticker']['last_price'][0] )
-    except Exception as exc:
-        log.warning( f"Exception getting ticker:\n{exc}\n{response.json()}\n"
-                     f"last_price={last_price}" )
+class PriceGetter():
+    def __init__(self):
+        self.prev_price = None
 
-    return last_price
+    def get_return(self) -> Optional[float]:
+        """Query ticker url return last price"""
+        last_price = None
+        try:
+            response = requests.get(CFG.ticker_url)
+            last_price = float( response.json()['ticker']['last_price'][0] )
+            if self.prev_price is not None:
+                return_ = (last_price - self.prev_price) / self.prev_price
+                self.prev_price = last_price
+                return return_
+
+        except Exception as exc:
+            log.warning( f"Exception getting ticker:\n{exc}\n{response.json()}\n"
+                         f"last_price={last_price}" )
+            self.prev_price = None
+
+        return None
 
 
-def _get_key( stream_name: str, keys_file: str) -> str:
+def _get_write_key( stream_name: str, keys_file: str) -> str:
     with open( keys_file ) as f_in:
         obj = json.load(f_in)
         return obj[stream_name]
