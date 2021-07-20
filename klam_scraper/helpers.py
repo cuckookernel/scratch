eed0bc1cf7686250bb3d3a9b393acf3c00d0a4c1
Teo Restrepo <mateini@gmail.com>
2021-07-19 23:04:13 -0500

    images scraper

---
+++ klam_scraper/helpers.py
@@ -0,0 +1,115 @@
+
+import sys
+from pathlib import Path
+from hashlib import sha256
+
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as exp_cond
+from selenium.common.exceptions import TimeoutException
+
+from sqlalchemy.engine import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from typing import Callable, Any
+
+URL = str
+# %%
+
+
+def start_driver( executable_path: Path ) -> WebDriver:
+    """Starts the external webdriver that opens Chrome window and connects to it"""
+    # download_path.mkdir(parents=True, exist_ok=True)
+    # l_info(f'country_2let={country_2let} Download path is: {download_path}')
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--start-maximized")
+    # chrome_options.add_argument(f"--lang={country_2let}")
+
+    # preference = {'download.default_directory': str(download_path),
+    #              "safebrowsing.enabled": "false"}
+    # chrome_options.add_experimental_option('prefs', preference)
+
+    driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
+
+    return driver
+    # %%
+
+
+def wait_for_and_get(driver: WebDriver, xpath: str,
+                     priority: Callable[ [Any], float] = None, timeout: float = 10):
+    """Fait for an element to appear and return it
+    if more than one element found, order using the priority function and"""
+    try:
+        WebDriverWait(driver, timeout).until(
+            exp_cond.presence_of_element_located((By.XPATH, xpath)))
+    except TimeoutException as exc:
+        print(f'timeout {timeout} when waiting for:\n{xpath}')
+        raise exc
+
+    els = driver.find_elements_by_xpath(xpath)
+
+    if priority:
+        els = sorted(els, key=priority)
+    # print( [el.text for el in els] )
+    return els[0]
+    # %%
+
+
+def l_info(*args):
+    """just print stuff to the console"""
+    print(*args, file=sys.stderr)
+
+
+def runpyfile( path, **kwargs ):
+    """A wrapper around pycharms own runfile function so that we can import it
+    from scripts and avoid 'undefined variable runfile' errors from code checkers"""
+    # noinspection PyUnresolvedReferences
+    runfile( path, **kwargs )  # pylint: disable=undefined-variable
+
+    # %%
+
+Base = declarative_base()
+# %%
+
+
+def create_db_engine():
+    # %%
+    engine = create_engine( 'sqlite:///klam_scraper.db' )
+
+    return engine
+
+
+
+
+def create_schema( engine ):
+    # %%
+    Base.metadata.drop_all(engine)
+    # %%
+    Base.metadata.create_all( engine )
+    # %%
+    Base.metadata.tables
+    # %%
+
+class DownloadedImage(Base):
+    """Defines a record representing a downloaded image"""
+    __tablename__ = 'downloaded_images'
+    id = Column(Integer, primary_key=True)
+    product_url = Column(String)
+    source_url = Column(String)
+    filename = Column(String)
+    filesize = Column(Integer)
+    mime_type = Column(String)
+    width = Column(Integer)
+    height = Column(Integer)
+
+
+# %%
+
+def make_file_path( base_path: Path, img_url: URL ) -> Path:
+    return base_path / (sha256(img_url.encode('utf8')).hexdigest()[:24] + Path(img_url).suffix)
+    # %%
---
+++ klam_scraper/scraper.py
@@ -0,0 +1,116 @@
+"""Scrape images from a site starting from a search results page (START_URL):
+Example:
+
+https://kunastores.com/peru/category/poncho/14/
+
+Conventions:
+_xp = XPath
+"""
+
+import os
+from typing import List
+from pathlib import Path
+from importlib import reload
+from time import sleep
+import requests
+
+from klam_scraper.helpers import WebDriver, DownloadedImage, make_file_path, URL, sessionmaker
+from klam_scraper import helpers as hp
+from PIL import Image
+import pandas as pd
+
+START_URL = "https://kunastores.com/peru/category/poncho/14/"
+CHROME_DRIVER_PATH = Path(os.getenv('HOME')) / "chromedriver"
+IMAGES_PATH = Path('./images')
+
+# %%
+
+
+def _interactive_testing():
+    # %%
+    reload( hp )
+    hp.runpyfile('klam_scraper/scraper.py')
+    # %%
+    IMAGES_PATH.mkdir(exist_ok=True, parents=True)
+    # %% DB Connection setup
+    engine = hp.create_db_engine()
+    Session = sessionmaker( bind=engine )
+    db_session = Session()
+    # %%
+    print( "starting chrome driver ...")
+    driver = hp.start_driver(executable_path=CHROME_DRIVER_PATH)
+    # %%
+    product_urls = get_product_urls(driver, start_url=START_URL )
+    # %%
+    print( f'{len(product_urls)} product urls found')
+    [ print(f"\t{url}") for url in product_urls ]
+
+    for prod_url in product_urls:
+        download_images_from_product_page( driver, prod_url, db_session )
+    # %%
+    df = pd.read_sql('select * from downloaded_images', engine )
+    # %%
+
+
+def get_product_urls( driver: WebDriver, start_url: str ) -> List[URL]:
+    """get all product urls linked from a product list / grid"""
+    driver.get( start_url )
+
+    for _ in range(2):
+        load_more_button_xp = "//div[contains(@class, 'products-list__products__load-more')]"
+        load_more_button = hp.wait_for_and_get( driver, load_more_button_xp )
+
+        load_more_button.click()
+        # %% Wait for more results to load
+        sleep(5)
+
+    result_links_xp = "//div[contains(@class, 'products-list__products')]//a"
+    result_links = driver.find_elements_by_xpath(result_links_xp)
+
+    product_urls = [ link.get_attribute('href') for link in result_links ]
+
+    return product_urls
+
+
+def download_images_from_product_page( driver: WebDriver, prod_url: URL ):
+    print(f"Visiting product page: {prod_url}")
+    driver.get(prod_url)
+
+    thumbnail_xp = "//img[contains(@class, 'image-gallery-thumbnail-image')]"
+    hp.wait_for_and_get(driver, thumbnail_xp)
+
+    thumbnails = driver.find_elements_by_xpath(thumbnail_xp)
+    print( f"{len(thumbnails)} thumbnails found, now downloading images" )
+
+    image_urls = [ thumbnail.get_attribute('src') for thumbnail in thumbnails ]
+
+    for img_url in image_urls:
+        download_img_to_local( img_url, prod_url, db_session )
+    # %%
+
+
+def download_img_to_local( img_url: URL, prod_url: URL, session ):
+
+    fpath = make_file_path( IMAGES_PATH, img_url )
+
+    if fpath.exists():
+        print("Image already in local disk, not downloading again:")
+
+        with fpath.open( "rb" ) as f_in:
+            data = f_in.read()
+    else:
+        resp = requests.get( img_url )
+        data = resp.content  # bytes
+
+        with fpath.open( "wb" ) as f_out:
+            f_out.write( data )
+
+        img = Image.open(fpath)
+
+        dl = DownloadedImage( product_url=prod_url, source_url=img_url,
+                              filename=str(fpath), filesize=len( data ),
+                              mime_type=resp.headers['Content-Type'],
+                              width=img.size[0], height=img.size[1] )
+        session.add(dl)
+        session.commit()
+        # %%
