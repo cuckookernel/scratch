a4e2108e1163ff739ee117dc1eac97c90e5d008a
Teo Restrepo <mateini@gmail.com>
2021-11-14 19:06:32 -0500

    img reframe resize

--- image_reframe_resize/img_reframe_rsz.py
+++ image_reframe_resize/img_reframe_rsz.py
@@ -1,8 +1,10 @@
 """A simple app to reframe an resize pictures"""
 
-from typing import List, Tuple
+from typing import List, Tuple, Callable
+from more_itertools import first_true
 import sys
 import time
+from enum import Enum
 from pathlib import Path
 from dataclasses import dataclass
 
@@ -70,6 +72,12 @@ def scale_to_height( img: Surface, target_height: int ) -> Surface:
     # else:
 
 
+class EScrollMode(Enum):
+    "whether mouse wheel scroll causes movement of frame or zoom in/out"
+    MOVE = 0
+    ZOOM = 1
+
+
 class AppState:
     """The apps moving parts"""
     def __init__( self ):
@@ -81,6 +89,8 @@ class AppState:
         self.img_idx = 0
         self.geom_inc = 1.05
         self.move_inc = 8
+
+        self.scroll_mode: EScrollMode = EScrollMode.MOVE
 
         self._load_source_img_fps()
 
@@ -94,11 +104,18 @@ class AppState:
                                                                  CFG.window_size[1] - 100])
 
         self._load_img()
-        img_width = self.image_scaled.get_size()[0]
 
-        self.frame = pg.Rect( [0, 0, img_width - 2, img_width / CFG.target_proportion() - 2] )
+        self.frame = self._center_frame_full_width()
         self._draw_image_and_frame()
         self._draw_buttons()
+        _print_help( print )
+
+    def _center_frame_full_width( self ) -> pg.Rect:
+        img_width, img_height = self.image_scaled.get_size()
+
+        frame_height = img_width / CFG.target_proportion()
+        vertical_margin = (img_height - frame_height) / 2
+        return pg.Rect( [0, vertical_margin, img_width, frame_height] )
 
     def _load_img( self ):
         self.orig_image_fp = self.source_img_fps[self.img_idx]
@@ -113,7 +130,7 @@ class AppState:
 
             self.screen.fill( BLACK )
 
-            img_w = self.image.get_width()
+            img_w = self.image_scaled.get_width()
             img_x = self.screen.get_width() / 2 - img_w / 2
             self.screen.blit( self.image, (img_x, 50) )
 
@@ -128,7 +145,7 @@ class AppState:
             if event.type == pg.QUIT:
                 sys.exit(0)
 
-            if event.type in (pg.MOUSEBUTTONDOWN, pg.MOUSEWHEEL) :
+            if event.type in (pg.MOUSEBUTTONDOWN, pg.MOUSEWHEEL):
                 self._process_mouse_event(event)
 
             elif event.type == pg.KEYUP:
@@ -149,25 +166,42 @@ class AppState:
         elif event.type == pg.MOUSEWHEEL:
             # print( event )
             if event.y == +1:
-                self._reduce_frame()
+                if self.scroll_mode == EScrollMode.MOVE:
+                    self.frame.y -= self.move_inc
+                    self._draw_image_and_frame()
+                elif self.scroll_mode == EScrollMode.ZOOM:
+                    self._reduce_frame()
+
             elif event.y == -1:
-                self._enlarge_frame()
+                if self.scroll_mode == EScrollMode.MOVE:
+                    self.frame.y += self.move_inc
+                    self._draw_image_and_frame()
+                elif self.scroll_mode == EScrollMode.ZOOM:
+                    self._enlarge_frame()
 
     def _process_kb_event( self, event ):
-        if event.key == pg.K_UP:
-            self.frame.y -= self.move_inc
+        if event.key == pg.K_z:
+            if self.scroll_mode == EScrollMode.MOVE:
+                self.scroll_mode = EScrollMode.ZOOM
+            elif self.scroll_mode == EScrollMode.ZOOM:
+                self.scroll_mode = EScrollMode.MOVE
+
+            print( f'scroll mode: {self.scroll_mode.name}')
+
+        elif event.key == pg.K_UP:
+            self._move_frame( dy=-self.move_inc )
             self._draw_image_and_frame()
 
         elif event.key == pg.K_DOWN:
-            self.frame.y += self.move_inc
+            self._move_frame( dy=+self.move_inc )
             self._draw_image_and_frame()
 
         elif event.key == pg.K_RIGHT:
-            self.frame.x += self.move_inc
+            self._move_frame( dx=+self.move_inc )
             self._draw_image_and_frame()
 
         elif event.key == pg.K_LEFT:
-            self.frame.x -= self.move_inc
+            self._move_frame( dx=-self.move_inc )
             self._draw_image_and_frame()
 
         elif event.key == pg.K_PAGEUP:
@@ -187,6 +221,26 @@ class AppState:
 
         elif event.key == pg.K_KP_ENTER or event.key == pg.K_RETURN:
             self._save_resized_image()
+            self._img_skip(inc=+1)
+
+    def _move_frame( self, dx: int = 0, dy: int = 0):
+        img_w, img_h = self.image_scaled.get_size()
+
+        over_x = self.frame.x + self.frame.width + dx - img_w
+        if self.frame.x + dx < 0:
+            self.frame.x = 0
+        elif over_x > 0:
+            self.frame.x += dx - over_x
+        else:
+            self.frame.x += dx
+
+        over_y = self.frame.y + self.frame.height + dy - img_h
+        if self.frame.y + dy < 0:
+            self.frame.y = 0
+        elif over_y > 0:
+            self.frame.y += dy - over_y
+        else:
+            self.frame.y += dy
 
     def _enlarge_frame(self):
         self.frame.h *= self.geom_inc
@@ -206,6 +260,7 @@ class AppState:
         self.img_idx += inc
         self.img_idx = (self.img_idx + self.n_imgs) % self.n_imgs
         self._load_img()
+        self.frame = self._center_frame_full_width()
         self._draw_image_and_frame()
 
     def _load_source_img_fps( self ):
@@ -215,7 +270,15 @@ class AppState:
         self.source_img_fps = [ fpath for fpath in all_files
                                 if fpath.suffix.lower() in (".jpg", ".png")]
 
-        print( len( self.source_img_fps ), f" found:\n{self.source_img_fps}" )
+        print( len( self.source_img_fps ), f" images found" )
+
+        def _not_in_target_dir( tup: Tuple[int, Path] ):
+            return not _make_target_path(tup[1]).exists()
+
+        first_idx_not_in_target = first_true( enumerate(self.source_img_fps),
+                                              pred=_not_in_target_dir, default=None)
+
+        self.img_idx = first_idx_not_in_target[0] or 0
 
         self.n_imgs = len( self.source_img_fps )
 
@@ -228,8 +291,8 @@ class AppState:
         self.image = self.image_scaled.copy()
         pg.draw.lines( self.image, closed=True, color=pg.Color('blue'),
                        points=[ (frame.x, frame.y),
-                                (frame.x + frame.w, frame.y),
-                                (frame.x + frame.w, frame.y + frame.h),
+                                (frame.x + frame.w - 2, frame.y),
+                                (frame.x + frame.w - 2, frame.y + frame.h),
                                 (frame.x, frame.y + frame.h)],
                        width=2 )
 
@@ -264,10 +327,30 @@ class AppState:
 
         resized = (resize( cropped, out_sz_hw, order=3, anti_aliasing=True )
                    * 255.0).astype(np.uint8)
-        out_fpath = CFG.target_dir / (self.orig_image_fp.stem + '.jpg')
+        out_fpath = _make_target_path(self.orig_image_fp)
+
         print( f"saving resized image: {out_fpath}")
         imsave( out_fpath, resized, quality=CFG.jpeg_quality )
         # %%
+
+
+def _make_target_path( fpath: Path ) -> Path:
+    return CFG.target_dir / (fpath.stem + '.jpg')
+
+
+def _print_help( print_fun: Callable[[str], None] = print ):
+    lines = """
+        [z]        : switch scroll mode from move to zoom 
+        [up]       : move frame up / zoom out if in scroll mode
+        [down]     : move frame down / zoom in if in scroll mode
+        [left]     : move frame left
+        [right]    : move frame right
+        [PageUp]   : previous pic
+        [PageDown] : next pic
+    """.split("\n")
+
+    for line in lines:
+        print_fun( line.lstrip() )
 
 
 def _is_in_rect( pos: Tuple[int], rect: pg.Rect ) -> bool:
@@ -276,3 +359,14 @@ def _is_in_rect( pos: Tuple[int], rect: pg.Rect ) -> bool:
 
 # if __name__ == "__main__":
 #     _main()
+
+
+def temp():
+    # %%
+    import requests
+
+    resp  = requests.get( "https://sedeelectronica.antioquia.gov.co/publicaciones/227/solicitud-de-citas--tramite-de-pasaportes/")
+
+    "https://sedeelectronica.antioquia.gov.co/pasaporte/user/createAppointment/"
+    https: // sedeelectronica.antioquia.gov.co / pasaporte / user / createAppointment /
+    # %%
