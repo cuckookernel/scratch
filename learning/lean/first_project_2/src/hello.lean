006008c5a3f085c01b09479cdeb5c9f4a2a65960
mrestrepo-ecw@cricut.com <mateini@gmail.com>
2025-12-27 13:05:33 -0500

    learning lean

--- learning/lean/first_project_2/src/hello.lean
+++ learning/lean/first_project_2/src/hello.lean
@@ -1,4 +1,4 @@
-import init.data.bool
+-- import init.data.bool
 
 #check bool
 #check true
@@ -6,7 +6,51 @@ import init.data.bool
 #eval 0
 
 #check 0
-#check nat
+#check Nat
+
+#check Nat -> Nat
+
+#check (0, 1)
+
+#check Nat.succ
+
+
+def α : Type := Nat
+
+
+-- def β := Nat
+
+#check Type
+
+#check Type 1
+
+
+#check List
+
+#check List.{1}
+
+
+#check fun (x: Nat) => x + 5
+
+#check λ (x : Nat) => x + 5
+
+
+def β := fun (x : Nat) => x + 5
+
+def γ := λ (x : Nat) => x + 5
+
+#check β = γ
+
+
+def iden (α : Type) := λ (x : α) => x
+
+#check iden Nat 5
+
+def greater (x y : Nat) : Nat := if x > y then x else y
+
+
+#eval greater 5 3
+
 
 
 theorem test( p q : Prop )(hp : p)(hq : q) : p ∧ q ∧ p :=
@@ -15,6 +59,6 @@ theorem test( p q : Prop )(hp : p)(hq : q) : p ∧ q ∧ p :=
     apply And.intro
       exact hq
       exact hp
+    ;
 
 #check test
-
